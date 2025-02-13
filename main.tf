2c5fdcf08a14968ad82abd45bd3018813454bae3
Sagardev88 <sagar.devagire@ltimindtree.com>
2025-02-13 14:16:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+apiversion:
+kind: deployments
+metadata:
+	labels : App\
+	name: Sagar
+Spec:
+  Replicas: 2
+  Templates:
+	metadata of application
+
+  Container:
+	name:
+	image:
+	ports: 	
+	   Nodeports:
+	   Targetpots:
+	
